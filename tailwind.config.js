43a032fd793743c622c976b7805c3a50787bff78
Yakuz0r <geoffrey_meunier@live.be>
2023-04-20 10:28:39 +0200

    full project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    colors:{
+      'white':'#FFFFFF',
+      'gray-border':'#cfd0d1',
+      'gray':'#3a414a',
+      'blue':'#0804fc',
+      'hover-li':'#E6EEFF',
+      'main':'#fafcfe',
+      'green':'rgb(0, 128, 0)',
+    },
+    fontFamily: {
+    sans: ["Open Sans"],
+    },
+  },
+  plugins: [],
+}
+
